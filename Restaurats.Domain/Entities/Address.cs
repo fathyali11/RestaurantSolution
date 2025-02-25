799f285b19d0d9eaa22cd28460c0e72f0d94070d
fathy <fathy@FATHYALI>
2025-02-25 23:44:21 +0200

    Add project files.

---
+++ Restaurats.Domain/Entities/Address.cs
@@ -0,0 +1,7 @@
+﻿namespace Restaurats.Domain.Entities;
+public class Address
+{
+    public string City { get; set; } = string.Empty;
+    public string Street { get; set; } = string.Empty;
+    public string PostalCode { get; set; } = string.Empty;
+}
---
+++ Restaurats.Domain/Entities/Dish.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurats.Domain.Entities;
+public class Dish
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+
+    public int KiloCalories { get; set; }
+
+    public int RestaurantId { get; set; }
+}
