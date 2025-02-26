d32a9fc7efecd453d9c624c07af9f8d54818d79f
fathy <fathy@FATHYALI>
2025-02-26 20:56:40 +0200

    remove seeding data

--- Restaurats.API/Program.cs
+++ Restaurats.API/Program.cs
@@ -18,12 +18,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-using(var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var restaurantSeeder = services.GetRequiredService<IRestaurantSeeder>();
-    await restaurantSeeder.SeedAsync();
-}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
