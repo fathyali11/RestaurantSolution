ba41e1884924615462e281ab8de07488bba86445
fathy <fathy@FATHYALI>
2025-02-28 22:59:22 +0200

    add logger

--- Restaurats.Application/Restaurants/Commands/DeleteRestaurant/DeleteRestaurantCommandHandler.cs
+++ Restaurats.Application/Restaurants/Commands/DeleteRestaurant/DeleteRestaurantCommandHandler.cs
@@ -1,17 +1,25 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using Restaurats.Domain.Repositories;
 
 namespace Restaurats.Application.Restaurants.Commands.DeleteRestaurant;
-internal class DeleteRestaurantCommandHandler(IUnitOfWork unitOfWork) : IRequestHandler<DeleteRestaurantCommand, bool>
+internal class DeleteRestaurantCommandHandler(IUnitOfWork unitOfWork,ILogger<DeleteRestaurantCommandHandler>logger) : IRequestHandler<DeleteRestaurantCommand, bool>
 {
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly ILogger<DeleteRestaurantCommandHandler> _logger = logger;
     public async Task<bool> Handle(DeleteRestaurantCommand request, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Get Restaurant From Database");
         var restaurantFromDb = await _unitOfWork.Restaurant.GetByIdAsync(request.Id);
         if (restaurantFromDb is null)
+        {
+            _logger.LogInformation("Restaurant Not Found");
             return false;
+        }
+        _logger.LogInformation("try to delete restaurant from db");
         await _unitOfWork.Restaurant.DeleteAsync(restaurantFromDb);
         await _unitOfWork.SaveChangesAsync(cancellationToken);
+        _logger.LogInformation("restaurant was deleted from db");
         return true;
     }
 }
--- Restaurats.Application/Restaurants/Queries/GetRestaurant/GetRestaurantQueryHandler.cs
+++ Restaurats.Application/Restaurants/Queries/GetRestaurant/GetRestaurantQueryHandler.cs
@@ -1,20 +1,29 @@
 ﻿using MapsterMapper;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using Restaurats.Application.Restaurants.Dtos;
 using Restaurats.Domain.Repositories;
 
 namespace Restaurats.Application.Restaurants.Queries.GetRestaurant;
-internal class GetRestaurantQueryHandler(IUnitOfWork unitOfWork,IMapper mapper) : IRequestHandler<GetRestaurantQuery, RestaurantResponse?>
+internal class GetRestaurantQueryHandler(IUnitOfWork unitOfWork,
+    IMapper mapper,ILogger<GetRestaurantQueryHandler> logger) :
+    IRequestHandler<GetRestaurantQuery, RestaurantResponse?>
 {
     private readonly IUnitOfWork _unitOfWork=unitOfWork;
     private readonly IMapper _mapper=mapper;
+    private readonly ILogger<GetRestaurantQueryHandler> _logger = logger;
     public async Task<RestaurantResponse?> Handle(GetRestaurantQuery request, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Try To Get Restaurant From Database");
         var restaurant = await _unitOfWork
             .Restaurant.GetByIdAsync(request.Id);
 
         if (restaurant is null)
+        {
+            _logger.LogInformation("Restaurant Not Found");
             return null;
+        }
+        _logger.LogInformation("Restaurant Found");
         return _mapper.Map<RestaurantResponse>(restaurant!);
     }
 }
