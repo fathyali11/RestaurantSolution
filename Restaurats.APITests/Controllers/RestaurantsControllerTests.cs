4fb146397a7d86090b368e0cf7290ceabe785992
fathy <fathy@FATHYALI>
2025-03-18 14:34:43 +0200

    add get by id tests

--- Restaurats.APITests/Controllers/RestaurantsControllerTests.cs
+++ Restaurats.APITests/Controllers/RestaurantsControllerTests.cs
@@ -1,19 +1,34 @@
-﻿using System.Net;
+﻿using Restaurats.API.Controllers;
+using System.Net;
 using System.Net.Http.Json;
 using FluentAssertions;
 using MediatR;
+using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using Restaurats.APITests;
 using Restaurats.Application.Common;
 using Restaurats.Application.Restaurants.Dtos;
 using Restaurats.Application.Restaurants.Queries.GetAllRestaurants;
 using Xunit;
+using Azure.Core;
+using Restaurats.Domain.Entities;
+using Restaurats.Domain.Repositories;
+using System.Linq.Expressions;
 
 namespace Restaurats.API.Controllers.Tests;
 
 public class RestaurantsControllerTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
 {
-    private readonly HttpClient _httpClient=factory.CreateClient();
+    private readonly HttpClient _httpClient =
+        factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+            });
+        }).CreateClient();
     [Fact()]
     public async Task GetAll_WhenValidQuery_ShouldReturnStatusCode200()
     {
@@ -30,5 +45,25 @@ public class RestaurantsControllerTests(WebApplicationFactory<Program> factory) 
 
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
 
+    }
+
+    [Fact()]
+    public async Task Get_WhenIdIsValid_ShouldReturnStatusCode200()
+    {
+        var response=await _httpClient.GetAsync("/api/Restaurants/1");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    [Fact()]
+    public async Task Get_WhenIdIsNotValid_ShouldReturnStatusCode200()
+    {
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+        unitOfWorkMock.Setup(x=>x.Restaurant.GetByIdAsync(It.IsAny<Expression<Func<Restaurant, bool>>>(),null, It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Restaurant?)null);
+
+        var response = await _httpClient.GetAsync("/api/Restaurants/0");
+        var content= await response.Content.ReadAsStringAsync();
+        content.Should().Be($"{nameof(Restaurant)} with id: 0 doesn't exist");
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
 }
---
+++ Restaurats.APITests/FakePolicyEvaluator.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Http;
+
+namespace Restaurats.APITests;
+internal class FakePolicyEvaluator : IPolicyEvaluator
+{
+    public Task<AuthenticateResult> AuthenticateAsync(AuthorizationPolicy policy, HttpContext context)
+    {
+        var claimsPrincipal = new ClaimsPrincipal(
+            new ClaimsIdentity(
+                new[]
+                         {
+                             new Claim(ClaimTypes.NameIdentifier, "1"),
+                             new Claim(ClaimTypes.Role, "Admin")
+                         }, 
+                "FakeScheme")
+            );
+
+        var authenticationTicket = new AuthenticationTicket(claimsPrincipal, "FakeScheme");
+
+        return Task.FromResult(AuthenticateResult.Success(authenticationTicket));
+    }
+
+    public Task<PolicyAuthorizationResult> AuthorizeAsync(AuthorizationPolicy policy, AuthenticateResult authenticationResult, HttpContext context, object? resource)
+    {
+        return Task.FromResult(PolicyAuthorizationResult.Success());
+    }
+}
